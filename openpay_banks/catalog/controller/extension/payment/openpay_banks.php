2e86c8fee1066f009381fc9d68a3f90a5e1cdd91
Gerardo Mendez <gerardo.mendez@openpay.mx>
2024-07-24 17:59:12 -0600

    sending client ip in header request for banks & stores

--- openpay_banks/catalog/controller/extension/payment/openpay_banks.php
+++ openpay_banks/catalog/controller/extension/payment/openpay_banks.php
@@ -244,7 +244,8 @@ class ControllerExtensionPaymentOpenpayBanks extends Controller {
             curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);            
             curl_setopt($ch, CURLOPT_HTTPHEADER, array(
                 'Content-Type: application/json',
-                'Content-Length: '.strlen($data_string))
+                'Content-Length: '.strlen($data_string),
+                'X-Forwarded-For: '. $this->getClientIp())
             );
         }
         
@@ -381,6 +382,24 @@ class ControllerExtensionPaymentOpenpayBanks extends Controller {
         $mail->send();
     }
 
+    private function getClientIp() {
+        // Recogemos la IP de la cabecera de la conexión
+        if (!empty($_SERVER['HTTP_CLIENT_IP']))   
+        {
+          $ipAdress = $_SERVER['HTTP_CLIENT_IP'];
+        }
+        // Caso en que la IP llega a través de un Proxy
+        elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))  
+        {
+          $ipAdress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+        // Caso en que la IP lleva a través de la cabecera de conexión remota
+        else
+        {
+          $ipAdress = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ipAdress;
+      }
 }
 
 ?>
